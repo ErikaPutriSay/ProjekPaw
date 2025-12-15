ff139bbcae269c582467f459d0e1d86b9598a779
Jennifer2327240052 <jennifer_2327240052@mhs.mdp.ac.id>
2025-12-15 16:37:17 +0700

    push

---
+++ angular-login-dashboard/src/app/Auth/Login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../Services/auth.services';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  login() {
+    if (this.auth.login(this.username, this.password)) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      this.error = 'Username atau password salah';
+    }
+  }
+}
--- angular-login-dashboard/src/app/app.routes.ts
+++ angular-login-dashboard/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Auth/Login.component';
+import { DashboardComponent } from './Pages/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
